a5cd977b17371a9e85511708d81f22fda1664ecb
Austin Bao <austin_dun@outlook.com>
2023-08-13 18:10:17 -0600

    corner map works

--- main.py
+++ main.py
@@ -1,46 +1,79 @@
-import os
 import pyautogui
 import cv2
 import numpy as np
 from ultralytics  import YOLO
 
-# Specify resolution
-resolution = (1920, 1080)
+# So we know who is jungling
+selectedChampion = input("What Champion should I be looking out for?\n")
 
-codec = cv2.VideoWriter_fourcc(*"XVID")
-filename = "Recording.avi"
-fps = 60.0
-out = cv2.VideoWriter(filename, codec, fps, resolution)
 
-cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+def draw_rectangle(event,x,y,flags,param):
 
-cv2.resizeWindow("Live", 480, 270)
+    global pt1,pt2,topLeft_clicked,botRight_clicked
+
+    # get mouse click
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        if topLeft_clicked == True and botRight_clicked == True:
+            topLeft_clicked = False
+            botRight_clicked = False
+            pt1 = (0,0)
+            pt2 = (0,0)
+
+        if topLeft_clicked == False:
+            pt1 = (x,y)
+            topLeft_clicked = True
+            
+        elif botRight_clicked == False:
+            pt2 = (x,y)
+            botRight_clicked = True
+
+
+cv2.namedWindow("Live")
+cv2.setMouseCallback('Live', draw_rectangle) 
+cv2.resizeWindow("Live", 1920, 1080)
+
+pt1 = (0,0)
+pt2 = (0,0)
+topLeft_clicked = False
+botRight_clicked = False
+
+model = YOLO("C:/OpenCV/LoLMapVision/runs/detect/train3/weights/best.pt")
 
 while True:
-	# Take screenshot using PyAutoGUI
-	img = pyautogui.screenshot()
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	frame = np.array(img)
-	
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	# Write it to the output file
-	out.write(frame)
-	
-	cv2.imshow('Live', frame)
-	
-	if cv2.waitKey(1) == 27:
-		break
+    if topLeft_clicked:
+        cv2.circle(frame, center=pt1, radius=5, color=(0,0,255), thickness=-1)
+    
+    if topLeft_clicked and botRight_clicked:
+        cv2.rectangle(frame, pt1, pt2, (0, 0, 255), 2)
+        y1, x1 = pt1
+        y2, x2 = pt2
 
-out.release()
+        mapRegion = pyautogui.screenshot(region=(y1, x1, y2-y1, x2-x1))
+        Map = np.array(mapRegion)
+        Map = cv2.cvtColor(Map, cv2.COLOR_BGR2RGB)
+        
+        # I want to filter by "classes" with the selectedChampion input from the start
+        model.predict(Map, save=True, save_txt=True, save_conf=True, max_det=1)
+        # redict_img_path = "C:/OpenCV/LoLMapVision/runs/detect/predict/image0.jpg"
+        # new_img = cv2.imread(predict_img_path)
+
+        cv2.imshow("Map", Map)
+
+    
+    cv2.imshow('Live', frame)
+        
+    if cv2.waitKey(1) == 27:
+        break
+
 cv2.destroyAllWindows()
 
 
 
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-if __name__ == '__main__':
-    model = YOLO('yolov8n.pt')
-
-    model.train(data="C:/Users/baosh/LoLMapVision/MapDataset/data.yaml", epochs=150)
